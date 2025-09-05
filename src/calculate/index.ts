8132bca6309ba09d335343e729d818dbd56d72b0
Martin Elsaesser <marels@aon.at>
2025-09-05 22:06:01 +0200

    refactor: simplify type definitions for subtraction, multiplication, and division

--- src/calculate/index.ts
+++ src/calculate/index.ts
@@ -7,15 +7,10 @@ type AddT<A extends number[], B extends number[]> = [...A, ...B];
 type Add<A extends number, B extends number> = AddT<Num<A>, Num<B>>["length"];
 
 // subtract
-type SubT<A extends number[], B extends number[], TRes extends number[] = []> = ([
-	...TRes,
-	...B,
-] extends A ?
-	TRes
-:	SubT<A, B, [0, ...TRes]>) &
-	number[];
+type SubT<A extends number[], B extends number[], TRes extends number[] = []> =
+	[...TRes, ...B] extends A ? TRes : SubT<A, B, [0, ...TRes]>;
 
-type Sub<A extends number, B extends number> = SubT<Num<A>, Num<B>>["length"];
+type Sub<A extends number, B extends number> = (SubT<Num<A>, Num<B>> & number[])["length"];
 
 // multiply
 type MultT<
@@ -23,17 +18,12 @@ type MultT<
 	B extends number[],
 	CountA extends number[] = [],
 	TRes extends number[] = [],
-> = (A extends CountA ? TRes : MultT<A, B, AddT<[0], CountA>, AddT<B, TRes>>) & number[];
+> = A extends CountA ? TRes : MultT<A, B, AddT<[0], CountA>, AddT<B, TRes>>;
 
-type Mult<A extends number, B extends number> = MultT<Num<A>, Num<B>>["length"];
+type Mult<A extends number, B extends number> = (MultT<Num<A>, Num<B>> & number[])["length"];
 
 // divide
-type DivT<A extends number[], B extends number[], TRes extends number[] = []> = (MultT<
-	TRes,
-	B
-> extends A ?
-	TRes
-:	DivT<A, B, AddT<[0], TRes>>) &
-	number[];
+type DivT<A extends number[], B extends number[], TRes extends number[] = []> =
+	MultT<TRes, B> extends A ? TRes : DivT<A, B, AddT<[0], TRes>>;
 
 type calc = Add<Mult<2, 3>, 7>; // 2*3 +7
