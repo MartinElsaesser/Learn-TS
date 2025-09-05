fa2378969b50c7d97615beb1a298e3f62e6b83f2
Martin Elsaesser <marels@aon.at>
2025-09-05 18:48:18 +0200

    capitalize function

--- src/string-literals/casing.ts
+++ src/string-literals/casing.ts
@@ -1,4 +1,27 @@
-type MyCapitalize<T extends string> =
-	T extends `${infer First}${infer Rest}` ? `${Uppercase<First>}${Rest}` : T;
+import { expectTypeOf } from "expect-type";
+type CapitalizeRest<T extends string> =
+	T extends `${infer First}${infer Second}${infer Rest}` ?
+		First extends " " ?
+			`${First}${Uppercase<Second>}${CapitalizeRest<Rest>}`
+		:	`${First}${CapitalizeRest<`${Second}${Rest}`>}`
+	: T extends `${infer First}${infer Rest}` ? First
+	: "";
 
-type capitalized = MyCapitalize<"hello">;
+type MyCapitalize<T extends string> =
+	T extends `${infer First}${infer Rest}` ?
+		First extends " " ?
+			`${CapitalizeRest<T>}`
+		:	`${Capitalize<First>}${CapitalizeRest<Rest>}`
+	:	"";
+
+type capitalized = MyCapitalize<"   spaces ">;
+
+expectTypeOf<"A">().toEqualTypeOf<MyCapitalize<"a">>();
+expectTypeOf<"Be">().toEqualTypeOf<MyCapitalize<"be">>();
+expectTypeOf<"Three">().toEqualTypeOf<MyCapitalize<"three">>();
+expectTypeOf<" Space">().toEqualTypeOf<MyCapitalize<" space">>();
+expectTypeOf<"   Spaces ">().toEqualTypeOf<MyCapitalize<"   spaces ">>();
+expectTypeOf<"Lorem, Ipsum. Dolor Sit Amet.">().toEqualTypeOf<
+	MyCapitalize<"lorem, ipsum. dolor sit amet.">
+>();
+expectTypeOf<"">().toEqualTypeOf<MyCapitalize<"">>();
