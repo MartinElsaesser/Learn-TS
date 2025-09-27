89d74b1041cfc37134fa181ddc97c553c80cb67a
Martin Elsaesser <marels@aon.at>
2025-09-27 23:26:32 +0200

    move integer numbers into their own file

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,37 +1,5 @@
 import { expectTypeOf } from "expect-type";
 
-type Num<TNum extends number, TTup extends number[] = []> =
-	TTup["length"] extends TNum ? TTup : Num<TNum, [0, ...TTup]>;
-
-// add
-type AddT<A extends number[], B extends number[]> = [...A, ...B];
-
-type Add<A extends number, B extends number> = AddT<Num<A>, Num<B>>["length"];
-
-// subtract
-type SubT<A extends number[], B extends number[], TRes extends number[] = []> =
-	[...TRes, ...B] extends A ? TRes : SubT<A, B, [0, ...TRes]>;
-
-type Sub<A extends number, B extends number> = (SubT<Num<A>, Num<B>> & number[])["length"];
-
-// multiply
-type MultT<
-	A extends number[],
-	B extends number[],
-	CountA extends number[] = [],
-	TRes extends number[] = [],
-> = A extends CountA ? TRes : MultT<A, B, AddT<[0], CountA>, AddT<B, TRes>>;
-
-type Mult<A extends number, B extends number> = (MultT<Num<A>, Num<B>> & number[])["length"];
-
-// divide
-type DivT<A extends number[], B extends number[], TRes extends number[] = []> =
-	MultT<TRes, B> extends A ? TRes : DivT<A, B, AddT<[0], TRes>>;
-
-type Div<A extends number, B extends number> = (DivT<Num<A>, Num<B>> & number[])["length"];
-
-type calc = Add<Mult<2, 3>, 7>; // 2*3 +7
-
 // binary
 // 1000
 // 100
---
+++ src/math/integers.ts
@@ -0,0 +1,32 @@
+type Num<TNum extends number, TTup extends number[] = []> =
+	TTup["length"] extends TNum ? TTup : Num<TNum, [0, ...TTup]>;
+
+// add
+type AddT<A extends number[], B extends number[]> = [...A, ...B];
+
+type Add<A extends number, B extends number> = AddT<Num<A>, Num<B>>["length"];
+
+// subtract
+type SubT<A extends number[], B extends number[], TRes extends number[] = []> =
+	[...TRes, ...B] extends A ? TRes : SubT<A, B, [0, ...TRes]>;
+
+type Sub<A extends number, B extends number> = (SubT<Num<A>, Num<B>> & number[])["length"];
+
+// multiply
+type MultT<
+	A extends number[],
+	B extends number[],
+	CountA extends number[] = [],
+	TRes extends number[] = [],
+> = A extends CountA ? TRes : MultT<A, B, AddT<[0], CountA>, AddT<B, TRes>>;
+
+type Mult<A extends number, B extends number> = (MultT<Num<A>, Num<B>> & number[])["length"];
+
+// divide
+type DivT<A extends number[], B extends number[], TRes extends number[] = []> =
+	MultT<TRes, B> extends A ? TRes : DivT<A, B, AddT<[0], TRes>>;
+
+type Div<A extends number, B extends number> = (DivT<Num<A>, Num<B>> & number[])["length"];
+
+type calc = Div<Add<Mult<2, 3>, 6>, 4>; // ((2*3) +7) / 4
+//   ^?
