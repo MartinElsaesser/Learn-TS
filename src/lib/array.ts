44f07b10a442e8b1b867604753693966d1df4229
Martin Elsaesser <marels@aon.at>
2025-09-28 16:50:03 +0200

    add JoinStrings type

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,4 +1,5 @@
 import { expectTypeOf as expected } from "expect-type";
+import * as $String from "./string";
 export type ExcludeLastElement<T extends readonly unknown[]> =
 	T extends [infer F, ...infer R] ?
 		R extends [] ?
@@ -25,4 +26,24 @@ const testSuite2 = [
 	expected<"a">().toEqualTypeOf<OnlyLastElement<["a"]>>(),
 	expected<"b">().toEqualTypeOf<OnlyLastElement<["a", "b"]>>(),
 	expected<"e">().toEqualTypeOf<OnlyLastElement<["a", "b", "c", "d", "e"]>>(),
+];
+
+type JoinStrings<SArr extends string[], Separator extends string, _Acc extends string = ""> =
+	SArr extends [infer F extends string, ...infer R extends string[]] ?
+		JoinStrings<R, Separator, $String.Join<_Acc, Separator, F>>
+	:	_Acc;
+
+type LL = JoinStrings<["a", "b", "c"], ".">;
+//   ^?
+
+const testSuite3 = [
+	expected<"">().toEqualTypeOf<JoinStrings<[""], ".">>(),
+	expected<"">().toEqualTypeOf<JoinStrings<["", ""], ".">>(),
+	expected<"">().toEqualTypeOf<JoinStrings<["", "", ""], ".">>(),
+	expected<"a">().toEqualTypeOf<JoinStrings<["a", ""], ".">>(),
+	expected<"b">().toEqualTypeOf<JoinStrings<["", "b"], ".">>(),
+	expected<"a.b">().toEqualTypeOf<JoinStrings<["a", "b"], ".">>(),
+	expected<"a.b.c.d">().toEqualTypeOf<JoinStrings<["a", "b", "c", "d"], ".">>(),
+	expected<"a">().toEqualTypeOf<JoinStrings<["a"], ".">>(),
+	expected<"">().toEqualTypeOf<JoinStrings<[""], ".">>(),
 ];
